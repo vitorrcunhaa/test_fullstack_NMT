367141a1025d00fca98db531bd37fd38a2fb784d
Vítor Cunha <vitorrcunhaa@gmail.com>
2020-11-20 19:51:23 -0300

    Add customer model

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    name = models.CharField(max_length=70, blank=False, default='')
+    age = models.PositiveIntegerField()
+    city = models.CharField(max_length=200,blank=False, default='')
