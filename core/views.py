001b9d4b885398c720c569998e32d8844f0de859
Vítor Cunha <vitorrcunhaa@gmail.com>
2020-11-24 00:00:50 -0300

    Add dummy data when creating new users

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 
 from core.forms import SignUpForm
+from customers.customers_mock import customers_dummy_data
+from customers.models import Customer
 
 
 def index(request):
@@ -18,6 +20,8 @@ def register(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
+            for customer in customers_dummy_data:
+                Customer.objects.create(owner=user, name=customer['name'], age=customer['age'], city=customer['city'])
             return redirect('index')
 
         return render(request, 'register.html', {'form': form})
